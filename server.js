1c609fa30327e6907ed262dd81fae9ceb59cdf7d
alex <superalex_1104@163.com>
2018-08-22 22:16:33 +0800

    浅层封装koa http 的use和listen

---
+++ server.js
@@ -0,0 +1,21 @@
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200)
+//   res.end('hello imooc')
+// })
+
+// server.listen(9092, () => {
+//   console.log('server start on port 9092')
+// })
+
+const Woa = require('./application')
+const app = new Woa()
+
+app.use((req, res) => {
+  res.writeHead(200)
+  res.end('hello imooc')
+})
+app.listen(9092, () => {
+  console.log('server running on port 9092')
+})
