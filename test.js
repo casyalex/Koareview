b589d8e64aac1cf1485e10b60a31e680492b5031
alex <superalex_1104@163.com>
2018-08-24 21:29:40 +0800

    同步的compose

--- test.js
+++ test.js
@@ -1,16 +1,47 @@
-let alex = {
-  _name: 'alex',
-  get name () {
-    // console.log('获取值')
-    // return '你好'
-    return this._name
-  },
-  set name (val) {
-    console.log('new name is ' + val)
-    this._name = val
+function add (x, y) {
+    return x+y
+}
+
+function double (z) {
+  return z*2
+}
+// const res1 = add(1, 2)
+// const res2 = double(res1)
+// console.log(res2)
+
+// const res3 = double(add(1, 2))
+// console.log(res3)
+
+const middlewares = [add, double]
+let len = middlewares.length
+function compose (midds) {
+  return (...args) => {
+    // 初始值
+    let res = midds[0](...args)
+    for(let i=1;i<len;i++){
+      res = midds[i](res)
+    }
+    return res
   }
 }
 
-console.log(alex.name)  // js在读取对象的值时，使用的是get方法，这个方法可以被修改
-alex.name = 'imooc'
-console.log(alex.name)
+const fn = compose(middlewares)
+const res = fn(1, 2)
+console.log(res)
+
+// let alex = {
+//   _name: 'alex',
+//   get name () {
+//     // console.log('获取值')
+//     // return '你好'
+//     return this._name
+//   },
+//   set name (val) {
+//     console.log('new name is ' + val)
+//     this._name = val
+//   }
+// }
+
+// console.log(alex.name)  // js在读取对象的值时，使用的是get方法，这个方法可以被修改
+// alex.name = 'imooc'
+// console.log(alex.name)
